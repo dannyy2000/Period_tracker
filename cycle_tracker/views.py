049415d74e7682da747777993c32b2f62a32b30a
dannyy2000 <akinsanyadaniel665@gmail.com>
2023-04-26 16:01:14 -0700

    new

--- cycle_tracker/views.py
+++ cycle_tracker/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from datetime import datetime, timedelta
 from django.views import View
@@ -17,16 +16,17 @@ class CycleDatesView(View):
         cycle_length = int(request.POST.get('cycle_length'))
 
         next_occurrence = previous_date + timedelta(days=cycle_length)
-        next_12_occurrences = [next_occurrence + timedelta(days=cycle_length * i) for i in range(1, 13)]
-        flow_date = previous_date
-        ovulation_date = previous_date + timedelta(days=cycle_length // 2)
-        # safe_periods = [(previous_date, ovulation_date - timedelta(days=1)),
-        #                 (ovulation_date + timedelta(days=1), next_occurrence - timedelta(days=1))]
+
+        next_12_occurrences = [next_occurrence + timedelta(days=cycle_length * i) for i in range(12)]
+
+        flow_date = next_occurrence
+
+        ovulation_date = previous_date + timedelta(days=cycle_length - 14)
 
         safe_periods = []
 
         for i in range(12):
-            end_cycle_dates = previous_date + timedelta(days=cycle_length)
+            end_cycle_dates = previous_date + timedelta(days=cycle_length * i)
             start_safe_periods = end_cycle_dates - timedelta(days=10)
             end_safe_periods = end_cycle_dates + timedelta(days=10)
             safe_periods.append((start_safe_periods, end_safe_periods))
@@ -42,4 +42,3 @@ class CycleDatesView(View):
         cycle.save()
 
         return render(request, 'cycle/cycle_tracker.html', {'cycle': cycle})
-
